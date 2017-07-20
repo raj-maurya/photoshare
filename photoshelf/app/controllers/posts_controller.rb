c8faa7ba026dd851b3b276500dee6d90a8f518de
Raj Kumar Maurya <rajkmaurya111@gmail.com>
2017-07-20 13:00:00 +0530

    adding user specific posts

--- photoshelf/app/controllers/posts_controller.rb
+++ photoshelf/app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+before_action :authenticate_user! 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
 def index
   @posts = Post.all
@@ -11,8 +13,13 @@ end
 
 
 def create
-  @post = Post.create(post_params)
+  if @post = Post.create(post_params)
+  flash[:success] = "Your post has been created!"
   redirect_to posts_path
+else
+  flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+     render :new
+   end
 end
 
 
@@ -22,10 +29,27 @@ def show
 end
 
 def edit
+    @post = Post.find(params[:id])
 end
 
 def update
-end  
+    # @post = Post.find(params[:id])
+    if @post.update(post_params)
+    flash[:success] = "Post updated."
+    redirect_to(post_path(@post))
+  else
+    flash.now[:alert] = "Update failed.  Please check the form."
+          render :edit
+        end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+end
+
+
 
 private
 
@@ -33,7 +57,10 @@ def post_params
   params.require(:post).permit(:image, :caption)
 end
 
+def set_post
+  @post = Post.find(params[:id])
 
+end
 
 
 end
--- photoshelf/app/models/post.rb
+++ photoshelf/app/models/post.rb
@@ -5,6 +5,8 @@ validates :image,  presence: true
 has_attached_file :image, styles: { :medium => "640x" }
 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+belongs_to :user
+validates :user_id, presence: true
 
 end
 #end
