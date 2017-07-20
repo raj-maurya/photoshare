b515b0620aa2b4ae83e4dbba704f16f6d3893484
Raj Kumar Maurya <rajkmaurya111@gmail.com>
2017-07-20 09:36:51 +0530

    adding posts/ view and show

--- photoshelf/app/models/post.rb
+++ photoshelf/app/models/post.rb
@@ -1,8 +1,10 @@
-class Post < ApplicationRecord
+#class Post < ApplicationRecord
   class Post < ActiveRecord::Base
-    validates :image, presence: true
 
-    has_attached_file :image, styles: { :medium => "640x" }
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+validates :image,  presence: true
+has_attached_file :image, styles: { :medium => "640x" }
+validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
 end
-end
+#end
